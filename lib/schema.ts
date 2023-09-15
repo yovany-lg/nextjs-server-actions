e7d8a663b41f9e42f85276ac295d0063ae8ff799
Yovany Luis <yovany.lg@gmail.com>
2023-09-15 12:07:24 -0600

    fixup! adds validation to `saveProduct`

--- lib/schema.ts
+++ lib/schema.ts
@@ -3,3 +3,20 @@ import { z } from 'zod';
 export const ProductPageParamsSchema = z.object({
   id: z.coerce.number().int().positive(),
 });
+
+export const SaveProductSchema = z.object({
+  id: z.coerce.number().int().positive(),
+  name: z
+    .string()
+    .trim()
+    .min(3, { message: 'Must be 3 or more characters long' })
+    .max(120, 'Must be 255 or less characters long'),
+  price: z.coerce
+    .number()
+    .nonnegative({ message: 'Must be a positive number' }),
+  description: z
+    .string()
+    .trim()
+    .max(255, 'Must be 255 or less characters long'),
+  image: z.string().trim().url({ message: 'Must be a valid URL' }),
+});
