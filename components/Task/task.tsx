27ebe39818fcf2305758bbcdf8978ef6aeb509d4
Yovany Luis <yovany.lg@gmail.com>
2023-10-10 22:18:50 -0600

    adds toggle action to the Task component

--- components/Task/task.tsx
+++ components/Task/task.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import { Task } from '@prisma/client';
 import { Checkbox } from '../ui/checkbox';
 import { Label } from '../ui/label';
+import { toggleTask } from '@/app/tasks/actions';
 
 export default function TaskComponent({ task }: { task: Task }) {
   const { done, id, title } = task;
@@ -8,7 +11,13 @@ export default function TaskComponent({ task }: { task: Task }) {
 
   return (
     <div className="rounded-md border px-4 py-2 font-mono text-sm shadow-sm flex gap-2 items-center w-full">
-      <Checkbox id={taskId} checked={done} />
+      <Checkbox
+        id={taskId}
+        checked={done}
+        onClick={async () => {
+          await toggleTask(id, !done);
+        }}
+      />
       <Label htmlFor={taskId}>{title}</Label>
     </div>
   );
