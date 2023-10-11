dc504bc294d9a247d1fc03f030ec47eeac430ec0
Yovany Luis <yovany.lg@gmail.com>
2023-10-10 22:18:10 -0600

    updates the task-form component

--- components/Task/task-form.tsx
+++ components/Task/task-form.tsx
@@ -1,20 +1,9 @@
-import prisma from '@/lib/db';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
-import { revalidatePath } from 'next/cache';
+import { createTask } from '@/app/tasks/actions';
 
 export default function TaskForm() {
-  async function createTask(formData: FormData) {
-    'use server';
-    await prisma.task.create({
-      data: {
-        title: formData.get('title') as string,
-      },
-    });
-    revalidatePath('/tasks');
-  }
-
   return (
     <form action={createTask}>
       <div className="flex flex-col gap-y-2 mb-4">
